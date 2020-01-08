624afc9dae44409f05a8381150796e921cb5bf01
martoalalu <martinalalu@gmail.com>
2020-01-08 16:00:11 -0300

    Update historias-calles.R

--- historias-calles.R
+++ historias-calles.R
@@ -1,6 +1,7 @@
 library(httr)
 library(tidyverse)
 library(splitstackshape)
+library(WikidataR)
 
 url="https://raw.githubusercontent.com/martoalalu/calles-ba/master/data/calles-actuales.txt"
 request <- GET(url = url)
@@ -25,37 +26,46 @@ df<-rename(df,calle=matrix.unlist.response_split...ncol...length.response_split.
 glosario<- cSplit(df, "calle", sep="\n")
 calles <- read.csv("https://raw.githubusercontent.com/martoalalu/calles-ba/master/data/callejero.csv", encoding = 'UTF-8')
 
-glosario$calle_01<-chartr('ÁÉÍÓÚ','AEIOU',glosario$calle_01)
+glosario$calle_01<-chartr('ÁÉÍÓÚÜ','AEIOUU',glosario$calle_01)
 calles$nomoficial <- toupper(calles$nomoficial)
 
 calles_glosario <- left_join(calles, glosario, by = c("nomoficial"="calle_01"))
 
-#####
-library(tidyverse)
-library(readtext)
-library(tm)
-
-#Importamos el libro de las calles y el listado de calles
-glosario <- readtext("https://raw.githubusercontent.com/martoalalu/calles-ba/master/data/calles-actuales.txt", encoding = 'UTF-8')
-calles <- read.csv("https://raw.githubusercontent.com/martoalalu/calles-ba/master/data/callejero.csv", encoding = 'UTF-8')
-
-#Pasamos a mayusculas los textos
-glosario$text <- toupper(glosario$text)
-calles$nomoficial <- toupper(calles$nomoficial)
+#Extraemos el año de la ordenanza / ley a través del cual se dio nombre a esa calle
+calles_glosario <- calles_glosario %>% 
+  mutate(anio_calle=str_extract(calle_02, pattern = '\\d{4}'))
 
 
-texto <- calles$nomoficial[1]
 
 
-#Todos los titulos de las calles empiezan con \n\n y luego el nombre de la calle
-enc <- "\n\n"
-fin <- ".\n"
+# Traemos el libro con la descripción de las calles
+url="https://raw.githubusercontent.com/martoalalu/calles-ba/master/data/calles-antiguas.txt"
+request <- GET(url = url)
 
-calles <- calles %>% 
-  mutate(regex=paste0(enc,nomoficial))
+request$status_code
+response <- content(request, as = "text", encoding = "UTF-8")
+response
+#Splitteamos
+response_split <- strsplit(response, "\n\n")
 
-#Probamos detectando la calle 11 de Septiembre y la ubicación exacta
-texto2 <- paste0(enc,"11 DE SEPTIEMBRE")
+#Probamos
+response_split[[1]][2007]
 
-str_detect(glosario$text, texto2)
-str_locate(glosario$text, texto2)
+#Pasamos a DF
+df_bio <- data.frame(matrix(unlist(response_split), ncol =length(response_split), byrow=T))
+
+#Reonmbramos columna
+df_bio<-rename(df_bio,calle=matrix.unlist.response_split...ncol...length.response_split...)
+
+#Spliteamos para quedarnos con una columna que tenga el nombre de calle
+
+bio<- cSplit(df_bio, "calle", sep="\n")
+bio$calle_01<-chartr('ÁÉÍÓÚÜ','AEIOUU',bio$calle_01)
+
+View(bio[1:20])
+
+
+
+
+
+str_locate(bio$calle_01, "-")
